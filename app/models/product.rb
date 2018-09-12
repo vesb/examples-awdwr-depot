e12d8adfc305427faa21b0b4dc0bfeafa79fc201
Veselin Borisov <github@webtech4all.com>
2018-09-12 14:44:43 +0300

    Iteration B1: Make basic product's field validation
    After Iteration B1 we have added some validations to sanitize data
    and products in database and also rework tests according to changes.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  validates :description, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, presence: true, uniqueness: true
+  validates :image_url, presence: true, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
