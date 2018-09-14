cc1ee45c52514a2f8829c7c3b2f9f8833cfd55db
Veselin Borisov <github@webtech4all.com>
2018-09-14 11:51:52 +0300

    Iteration D1: Introduce session and concerns (shopping cart)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_091908) do
+ActiveRecord::Schema.define(version: 2018_09_14_082447) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
