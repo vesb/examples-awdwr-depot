fee2ca15373dd26f452fa27999d05ac4499543f4
Veselin Borisov <github@webtech4all.com>
2018-09-14 14:12:33 +0300

    Iteration E1: DB migration with columns and data, rollbacks (combine items in cart)

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,7 +28,7 @@ class LineItemsController < ApplicationController
   def create
     session[:store_index_visit_counter] = 1 unless session[:store_index_visit_counter].nil?
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
