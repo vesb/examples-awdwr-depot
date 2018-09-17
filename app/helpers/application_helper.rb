1a9b5e1e5f4e730f25fa0e61a77124b42f416471
Veselin Borisov <github@webtech4all.com>
2018-09-18 01:35:24 +0300

    Iteration F4: jQuery effects and view helpers introduction

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def hidden_dif_if(condition, attributes = {}, &block)
+    if condition
+      attributes['style'] = 'display: none;'
+    else
+      attributes.except!('style')
+    end
+
+    content_tag('div', attributes, &block)
+  end
 end
