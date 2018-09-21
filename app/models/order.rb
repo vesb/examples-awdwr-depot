95d71e8744d9ca6df9462ebf5790dc1b02ae2691
Veselin Borisov <github@webtech4all.com>
2018-09-21 21:02:57 +0300

    Rename order's pay_type to payment_type and move it to dbase
    Due to bad naming order.pay_type need to be renamed to payment_type.
    Also we need to move payment_types to database not in model's const

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,19 +1,25 @@
 class Order < ApplicationRecord
   has_many :line_items, dependent: :destroy
 
-  enum pay_type: {
-    'Check' => 0,
-    'Credit card' => 1,
-    'Purchase order' => 2,
-  }
-
   validates :name, :address, :email, presence: true
-  validates :pay_type, inclusion: pay_types.keys
+  validates :payment_type, inclusion: {
+    in: PaymentType.all.map { |payment_type| payment_type.id }
+  }
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
       item.cart_id = nil
       line_items << item
     end
+  end
+
+  def available_payment_types
+    if @available_payment_types.nil?
+      @available_payment_types = PaymentType.all.map { |payment_type|
+        [payment_type.name, payment_type.id]
+      }.to_h
+    end
+
+    @available_payment_types
   end
 end
--- app/views/products/who_bought.atom.builder
+++ app/views/products/who_bought.atom.builder
@@ -27,7 +27,7 @@ atom_feed do |feed|
             xhtml.th number_to_currency order.line_items.map(&:total_price).sum
           end
         end
-        xhtml.p "Paid by #{order.pay_type}"
+        xhtml.p "Paid by #{order.payment_type}"
       end
       entry.author do |author|
         author.name order.name
