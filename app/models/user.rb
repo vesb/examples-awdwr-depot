65b1fac98d4a080578eb038f683a7a229b43f31e
Veselin Borisov <github@webtech4all.com>
2018-10-05 16:25:15 +0300

    Iteration I4: Auth user menus, ActiveRecord's hooks, transactions, use rails console in app context

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
+  after_destroy :ensure_an_admin_remains
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  class Error < StandardError
+  end
+
+  private
+
+  def ensure_an_admin_remains
+    if User.count.zero?
+      # ActiveRecord::Rollback exception is not passed on in chain
+      raise Error.new 'Can not delete the last user'
+    end
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -44,5 +44,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to users_url
+    assert_equal flash[:notice], 'User was successfully destroyed.'
+  end
+
+  test 'should not destroy the last user' do
+    delete user_url(users(:two))
+
+    assert_difference('User.count', 0) do
+      delete user_url(users(:one))
+
+      assert_redirected_to users_url
+      assert_equal flash[:notice], 'Can not delete the last user'
+    end
   end
 end
