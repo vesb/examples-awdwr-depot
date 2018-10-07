c7610eab8f7089a2eb20154a071e760d3be83d5b
Veselin Borisov <github@webtech4all.com>
2018-10-08 01:25:29 +0300

    Iteration I: Playtime(3) Handle http_auth and diff unauthorized formats

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,22 @@ class ApplicationController < ActionController::Base
   protected
 
   def authorize
+    unless request.format.html?
+      authenticate_or_request_with_http_basic('Admin') do |username, password|
+        user = User.find_by(name: username)
+        if user.try(:authenticate, password) ||
+          user&.authenticate(params[:password]) # same thing diff syntax
+          session[:user_id] = user.id
+        end
+      end
+    end
     unless User.find_by(id: session[:user_id])
-      redirect_to login_url, notice: 'Please log in'
+      respond_to do |format|
+        format.html { redirect_to login_url, notice: 'Please log in' }
+        format.json { render json: 'Not authorized', status: 401 }
+        format.atom { render json: 'Not authorized', status: 401 }
+        return unless request.format.html?
+      end
     end
   end
 end
