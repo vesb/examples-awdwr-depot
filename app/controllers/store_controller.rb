7d5238571e13dc7bd8f54e080b5e383da142c110
Veselin Borisov <github@webtech4all.com>
2018-10-09 01:05:26 +0300

    I18n switcher handled by param's name in store index

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,11 +4,15 @@ class StoreController < ApplicationController
   before_action :set_cart
 
   def index
-    @products = Product.order(:title)
-    if session[:store_index_visit_counter].nil?
-      session[:store_index_visit_counter] = 1
+    if params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
     else
-      session[:store_index_visit_counter] += 1
+      @products = Product.order(:title)
+      if session[:store_index_visit_counter].nil?
+        session[:store_index_visit_counter] = 1
+      else
+        session[:store_index_visit_counter] += 1
+      end
     end
   end
 end
