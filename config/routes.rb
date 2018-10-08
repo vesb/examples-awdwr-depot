0809f94cd5ad5918fbedda3af5beb1d8c2bd2a99
Veselin Borisov <github@webtech4all.com>
2018-10-08 22:59:58 +0300

    Configuring I18n locales and routes in scope

--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,19 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
 
   resources :products do
     # Iteration G2 atom feeds
     get :who_bought, on: :member
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
 
   # CUSTOM routes
 
